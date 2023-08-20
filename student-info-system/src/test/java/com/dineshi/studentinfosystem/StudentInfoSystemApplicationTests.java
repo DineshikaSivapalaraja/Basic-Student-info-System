bb14ccea3c53b81aa9209bd21a4d4d7b878ce9a7
Dineshika <99dinuraja@gmail.com>
2023-08-20 13:11:44 +0530

    Initial commit

---
+++ student-info-system/src/test/java/com/dineshi/studentinfosystem/StudentInfoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dineshi.studentinfosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentInfoSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
