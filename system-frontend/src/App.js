352ec5f5285410abd636c34d25f17d578569441c
Dineshika <99dinuraja@gmail.com>
2023-09-24 05:28:56 +0530

    CRUD operations & backend implemented

---
+++ system-frontend/src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import AppBar from './components/AppBar';
+import AddStudent from './components/addStudent';
+import StudentDetails from './components/studentDetails';
+import { initializeNavigationFunctions } from './components/navigation';
+
+function App() {
+  // State to track which component to render
+  const [showAddStudent, setShowAddStudent] = useState(true);
+
+  // Function to navigate to Student Details
+  const navigateToStudentDetails = () => {
+    setShowAddStudent(false);
+  };
+
+  // Function to navigate back to Add Student
+  const navigateToAddStudent = () => {
+    setShowAddStudent(true);
+  };
+
+  // Initialize navigation functions
+  initializeNavigationFunctions(navigateToStudentDetails, navigateToAddStudent);
+
+  return (
+    <div className="App">
+      <AppBar/>
+      {showAddStudent ? (
+        <AddStudent/>
+      ) : (
+        <StudentDetails />
+      )}
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+// import React from 'react';
+// import Student from './components/Student';
+// import Student from './components/student1';
+// import Students from './components/student2';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//         {/* Hey This is Dineshika! */}
+//         <AppBar/>
+//         {/* <Student/>
+//         <Students/> */}
+//     </div>
+//   );
+// }
+// export default App;
---
+++ system-frontend/src/components/Student.js
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from 'react';   
+import './Student.css';
+
+export default function Student() {
+    const [name, setName] = useState('');
+    const [stu_id, setStu_ID] = useState('');
+    const [degree, setDegree] = useState('');
+    const [year, setYear] = useState('');
+    const [students, setStudents] = useState([]);
+    const [showAddForm, setShowAddForm] = useState(false); // State to control display of add form
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+
+        const student = { name, stu_id, degree, year };
+
+    // To Add student details
+        try {
+            const response = await fetch("http://localhost:8080/Student/add", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(student)
+            });
+
+            if (response.ok) {
+                console.log("New Student added");
+            } else {
+                console.error("Failed to add student");
+            }
+        } catch (error) {
+            console.error("An error occurred:", error);
+        }
+    }
+// new
+    const toggleAddForm = () => {
+        setShowAddForm(!showAddForm);
+    }
+
+    // Display/Get all students details
+    const fetchStudents = () => {
+        fetch("http://localhost:8080/Student/getAll")
+            .then(res => res.json())
+            .then(result => {
+                setStudents(result);
+            });
+    }
+    useEffect(() => {
+        // Fetch students when the component mounts
+        fetchStudents();
+    }, []);
+
+    return (
+
+
+        <div className="container">
+            <button onClick={toggleAddForm}>Add Student</button>
+            {/* Display Student Details */}
+            <div className="Students">
+                <h1 className="title">Students Details</h1>
+                {students.map(Student => (
+                    <div className="student" key={Student.id}>
+                        <p>Id: {Student.id}</p>
+                        <p>Name: {Student.name}</p>
+                        <p>Student ID: {Student.stu_id}</p>
+                        <p>Degree: {Student.degree}</p>
+                        <p>Year: {Student.year}</p>
+                    </div>
+                ))}
+            </div>
+
+            {showAddForm && (
+                <div className="modal">
+                <div className="modal-content">
+                    <span className="close" onClick={toggleAddForm}>&times;</span>
+                    <h1 className="title">Add Student</h1>
+                    <form className="form">
+                        <input
+                        className="input"
+                        type="text"
+                        placeholder="Student Name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <input
+                        className="input"
+                        type="text"
+                        placeholder="Student ID"
+                        value={stu_id}
+                        onChange={(e) => setStu_ID(e.target.value)}
+                    />
+                    <input
+                        className="input"
+                        type="text"
+                        placeholder="Degree"
+                        value={degree}
+                        onChange={(e) => setDegree(e.target.value)}
+                    />
+                    <input
+                        className="input"
+                        type="number"
+                        placeholder="Year"
+                        value={year}
+                        onChange={(e) => setYear(e.target.value)}
+                    />
+                        <button className="button" type="submit" onClick={handleClick}>Submit</button>
+                    </form>
+                </div>
+                </div>
+            )}
+        </div>
+    );
+}
