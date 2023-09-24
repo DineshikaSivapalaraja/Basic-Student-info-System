6bf1280038d3ba9637e4a1e849087806e07adac8
Dineshika <99dinuraja@gmail.com>
2023-09-24 05:49:13 +0530

    Project completed with crud operation

--- system-frontend/src/components/navigation.js
+++
@@ -1,7 +0,0 @@
-export let navigateToStudentDetails = null;
-export let navigateToAddStudent = null;
-
-export function initializeNavigationFunctions(toStudentDetails, toAddStudent) {
-    navigateToStudentDetails = toStudentDetails;
-    navigateToAddStudent = toAddStudent;
-}
